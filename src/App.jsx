214c67a4c281f8ae9f3d278ece0aedfc5d3edb7c
Pyae Sone <pyaesone.perfect2014@gmail.com>
2023-09-16 18:27:27 +0800

    Build the card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import qrImg from './assets/image-qr-code.png'
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <Qr />
+      <Text />
+    </div>
+  )
+}
+
+const Qr = () => {
+  return (
+    <img src={qrImg}></img>
+  )
+}
+
+const Text = () => {
+  return (
+    <div>
+      <h2>Improve your front-end skills by building projects</h2>
+      <p>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+      </p>
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id='app'>
+      <Card />
+    </div>
   )
 }
 
